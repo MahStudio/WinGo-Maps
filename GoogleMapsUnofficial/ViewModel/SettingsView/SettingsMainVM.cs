d6a7082696caa44cbd5fbfbc185801fe745ad592
ngame <ngame@DESKTOP-HSA4VI4>
2018-01-24 16:28:01 +0330

    -Added settings view (blank yet)

---
+++ GoogleMapsUnofficial/ViewModel/SettingsView/SettingsMainVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapsUnofficial.ViewModel.SettingsView
+{
+    class SettingsMainVM : INotifyPropertyChanged
+    {
+        private bool _allowOverstretch;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool AllowOverstretch
+        {
+            get { return _allowOverstretch; }
+            set { _allowOverstretch = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("AllowOverstretch"); }
+        }
+
+        public SettingsMainVM()
+        {
+
+        }
+    }
+}
