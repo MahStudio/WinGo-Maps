3ff8da4d4b04e24c2df30c1427e80d0610000575
ngame <ngame@DESKTOP-HSA4VI4>
2018-02-21 03:10:09 +0330

    -Fix for location button. -Search bar width again set to 250 to fix hamburger menu issue in small screens

--- GoogleMapsUnofficial/View/OnMapControls/MyLocationUserControl.xaml.cs
+++ GoogleMapsUnofficial/View/OnMapControls/MyLocationUserControl.xaml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
-using Windows.Storage;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -21,55 +20,36 @@ namespace GoogleMapsUnofficial.View.OnMapControls
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var logfile = await KnownFolders.PicturesLibrary.CreateFileAsync("WhereIsCrash.Log", CreationCollisionOption.GenerateUniqueName);
             count++;
-            await FileIO.WriteTextAsync(logfile, "Count++;");
             await Task.Delay(350);
             await CoreWindow.GetForCurrentThread().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async delegate
             {
                 if (count == 0) return;
                 if (count == 1)
                 {
-                    await FileIO.WriteTextAsync(logfile, "Count is 1");
                     try
                     {
-                        await FileIO.WriteTextAsync(logfile, "try");
                         var accessStatus = await Geolocator.RequestAccessAsync();
-                        await FileIO.WriteTextAsync(logfile, "RequestAccessAsync");
                         if (accessStatus == GeolocationAccessStatus.Allowed)
                         {
-                            await FileIO.WriteTextAsync(logfile, "Allowed");
-                            if (MapViewVM.GeoLocate == null) return;
-                            await FileIO.WriteTextAsync(logfile, "GeoLocate is NOT NULL");
-                            if (MapViewVM.GeoLocate.LocationStatus == PositionStatus.Ready)
-                            {
-                                await FileIO.WriteTextAsync(logfile, "GeoLocate.LocationStatus == PositionStatus.Ready");
-                                Geoposition pos = await MapViewVM.GeoLocate.GetGeopositionAsync();
-                                await FileIO.WriteTextAsync(logfile, "GeoLocate.GetGeopositionAsync");
-                                if (pos == null) return;
-                                await FileIO.WriteTextAsync(logfile, "pos is NOT NULL");
-                                Geopoint snPoint = pos.Coordinate.Point;
-                                await FileIO.WriteTextAsync(logfile, "snPoint" + snPoint.Position.Latitude + "," + snPoint.Position.Longitude);
-                                var Map = MapView.MapControl;
-                                await Task.Delay(10);
-                                if (Map == null)
-                                    await FileIO.WriteTextAsync(logfile, "Map is NULL");
-                                else
-                                    await FileIO.WriteTextAsync(logfile, "Map is NOT NULL");
-                                Map.Center = snPoint;
-                                await FileIO.WriteTextAsync(logfile, "Map.Center");
-                                Map.ZoomLevel = 16;
-                                await FileIO.WriteTextAsync(logfile, "Map.ZoomLevel");
-                                MapViewVM.UserLocation.Location = snPoint;
-                                await FileIO.WriteTextAsync(logfile, "UserLocation.Location");
-                            }
+                            Geolocator geolocator = new Geolocator() { DesiredAccuracy = PositionAccuracy.High };
+
+                            Geoposition pos = await geolocator.GetGeopositionAsync();
+
+                            BasicGeoposition snPosition = new BasicGeoposition { Latitude = pos.Coordinate.Point.Position.Latitude, Longitude = pos.Coordinate.Point.Position.Longitude };
+                            Geopoint snPoint = new Geopoint(snPosition);
+                            await Task.Delay(10);
+                            var Map = MapView.MapControl;
+                            Map = View.MapView.MapControl;
+                            Map.Center = snPoint;
+                            Map.ZoomLevel = 16;
                         }
                     }
                     catch { }
                 }
                 else
                 {
-                    if (MapViewVM.Compass != null)
+                    if(MapViewVM.Compass != null)
                     {
                         MapViewVM.CompassEnabled = !MapViewVM.CompassEnabled;
                         if (MapViewVM.CompassEnabled)
