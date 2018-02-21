01d371bae43128fd0cd0974feda2875702e77832
ngame <ngame@DESKTOP-HSA4VI4>
2018-02-21 03:45:47 +0330

    Revert "-Multiple line for search"
    This reverts commit 61d09e6b9b0523a80c7b9d95171c0397df721d8b.

--- GoogleMapsUnofficial/View/OnMapControls/MyLocationUserControl.xaml.cs
+++ GoogleMapsUnofficial/View/OnMapControls/MyLocationUserControl.xaml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
-using Windows.Storage;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -21,47 +20,29 @@ namespace GoogleMapsUnofficial.View.OnMapControls
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var logfile = await KnownFolders.PicturesLibrary.CreateFileAsync("WhereIsCrash.Log", CreationCollisionOption.GenerateUniqueName);
             count++;
-            await FileIO.WriteTextAsync(logfile, "Count++;");
             await Task.Delay(350);
             await CoreWindow.GetForCurrentThread().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async delegate
             {
                 if (count == 0) return;
                 if (count == 1)
                 {
-                    await FileIO.WriteTextAsync(logfile, "Count is 1");
                     try
                     {
-                        await FileIO.WriteTextAsync(logfile, "try");
                         var accessStatus = await Geolocator.RequestAccessAsync();
-                        await FileIO.WriteTextAsync(logfile, "RequestAccessAsync");
                         if (accessStatus == GeolocationAccessStatus.Allowed)
                         {
-                            await FileIO.WriteTextAsync(logfile, "Allowed");
                             if (MapViewVM.GeoLocate == null) return;
-                            await FileIO.WriteTextAsync(logfile, "GeoLocate is NOT NULL");
                             if (MapViewVM.GeoLocate.LocationStatus == PositionStatus.Ready)
                             {
-                                await FileIO.WriteTextAsync(logfile, "GeoLocate.LocationStatus == PositionStatus.Ready");
                                 Geoposition pos = await MapViewVM.GeoLocate.GetGeopositionAsync();
-                                await FileIO.WriteTextAsync(logfile, "GeoLocate.GetGeopositionAsync");
                                 if (pos == null) return;
-                                await FileIO.WriteTextAsync(logfile, "pos is NOT NULL");
                                 Geopoint snPoint = pos.Coordinate.Point;
-                                await FileIO.WriteTextAsync(logfile, "snPoint" + snPoint.Position.Latitude + "," + snPoint.Position.Longitude);
                                 var Map = MapView.MapControl;
                                 await Task.Delay(10);
-                                if (Map == null)
-                                    await FileIO.WriteTextAsync(logfile, "Map is NULL");
-                                else
-                                    await FileIO.WriteTextAsync(logfile, "Map is NOT NULL");
                                 Map.Center = snPoint;
-                                await FileIO.WriteTextAsync(logfile, "Map.Center");
                                 Map.ZoomLevel = 16;
-                                await FileIO.WriteTextAsync(logfile, "Map.ZoomLevel");
                                 MapViewVM.UserLocation.Location = snPoint;
-                                await FileIO.WriteTextAsync(logfile, "UserLocation.Location");
                             }
                         }
                     }
@@ -69,7 +50,7 @@ namespace GoogleMapsUnofficial.View.OnMapControls
                 }
                 else
                 {
-                    if (MapViewVM.Compass != null)
+                    if(MapViewVM.Compass != null)
                     {
                         MapViewVM.CompassEnabled = !MapViewVM.CompassEnabled;
                         if (MapViewVM.CompassEnabled)
