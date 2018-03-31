d390def41449d11c375a494601fbd447f5fbadf3
ngame <ngame@DESKTOP-ESFCCS8>
2018-03-31 18:54:24 +0430

    -Added Roads only

--- GoogleMapsUnofficial/View/OnMapControls/ChangeViewUserControl.xaml.cs
+++ GoogleMapsUnofficial/View/OnMapControls/ChangeViewUserControl.xaml.cs
@@ -36,6 +36,7 @@ namespace GoogleMapsUnofficial.View.OnMapControls
             DefaultMapView.IsChecked = true;
             SatelliteMapView.IsChecked = false;
             HybridMapView.IsChecked = false;
+            RoadsOnlyView.IsChecked = false;
         }
 
         private void SatelliteMapView_Click(object sender, RoutedEventArgs e)
@@ -43,6 +44,7 @@ namespace GoogleMapsUnofficial.View.OnMapControls
             DefaultMapView.IsChecked = false;
             SatelliteMapView.IsChecked = true;
             HybridMapView.IsChecked = false;
+            RoadsOnlyView.IsChecked = false;
         }
 
         private void HybridMapView_Click(object sender, RoutedEventArgs e)
@@ -50,6 +52,15 @@ namespace GoogleMapsUnofficial.View.OnMapControls
             DefaultMapView.IsChecked = false;
             SatelliteMapView.IsChecked = false;
             HybridMapView.IsChecked = true;
+            RoadsOnlyView.IsChecked = false;
+        }
+
+        private void RoadsOnlyView_Click(object sender, RoutedEventArgs e)
+        {
+            DefaultMapView.IsChecked = false;
+            SatelliteMapView.IsChecked = false;
+            HybridMapView.IsChecked = false;
+            RoadsOnlyView.IsChecked = true;
         }
 
         private void DefaultMapView_Checked(object sender, RoutedEventArgs e)
@@ -141,6 +152,23 @@ namespace GoogleMapsUnofficial.View.OnMapControls
                     Map.TileSources.Add(new MapTileSource(dataSource) { AllowOverstretch = AllowOverstretch, IsFadingEnabled = FadeAnimationEnabled });
                 }
             }
+            else if(RoadsOnlyView.IsChecked.Value)
+            {
+                if (ShowTraffic.IsOn)
+                {
+                    Map.TileSources.Clear();
+                    var mapuri = "http://mt1.google.com/vt/lyrs=h@221097413,traffic&hl=" + AppCore.OnMapLanguage + "&z={zoomlevel}&x={x}&y={y}";
+                    var dataSource = new HttpMapTileDataSource(mapuri) { AllowCaching = true };
+                    Map.TileSources.Add(new MapTileSource(dataSource) { AllowOverstretch = AllowOverstretch, IsFadingEnabled = FadeAnimationEnabled });
+                }
+                else
+                {
+                    Map.TileSources.Clear();
+                    var mapuri = "http://mt1.google.com/vt/lyrs=h&hl=" + AppCore.OnMapLanguage + "&z={zoomlevel}&x={x}&y={y}";
+                    var dataSource = new HttpMapTileDataSource(mapuri) { AllowCaching = true };
+                    Map.TileSources.Add(new MapTileSource(dataSource) { AllowOverstretch = AllowOverstretch, IsFadingEnabled = FadeAnimationEnabled });
+                }
+            }
         }
 
         private void HybridMapView_Checked(object sender, RoutedEventArgs e)
@@ -156,6 +184,24 @@ namespace GoogleMapsUnofficial.View.OnMapControls
             {
                 Map.TileSources.Clear();
                 var mapuri = "http://mt1.google.com/vt/lyrs=y&hl=" + AppCore.OnMapLanguage + "&z={zoomlevel}&x={x}&y={y}";
+                var dataSource = new HttpMapTileDataSource(mapuri) { AllowCaching = true };
+                Map.TileSources.Add(new MapTileSource(dataSource) { AllowOverstretch = AllowOverstretch, IsFadingEnabled = FadeAnimationEnabled });
+            }
+        }
+
+        private void RoadsOnlyView_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ShowTraffic.IsOn)
+            {
+                Map.TileSources.Clear();
+                var mapuri = "http://mt1.google.com/vt/lyrs=h@221097413,traffic&hl=" + AppCore.OnMapLanguage + "&z={zoomlevel}&x={x}&y={y}";
+                var dataSource = new HttpMapTileDataSource(mapuri) { AllowCaching = true };
+                Map.TileSources.Add(new MapTileSource(dataSource) { AllowOverstretch = AllowOverstretch, IsFadingEnabled = FadeAnimationEnabled });
+            }
+            else
+            {
+                Map.TileSources.Clear();
+                var mapuri = "http://mt1.google.com/vt/lyrs=h&hl=" + AppCore.OnMapLanguage + "&z={zoomlevel}&x={x}&y={y}";
                 var dataSource = new HttpMapTileDataSource(mapuri) { AllowCaching = true };
                 Map.TileSources.Add(new MapTileSource(dataSource) { AllowOverstretch = AllowOverstretch, IsFadingEnabled = FadeAnimationEnabled });
             }
