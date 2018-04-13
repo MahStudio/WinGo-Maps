b7422449d9c47c94db6a81662edecc9c2fefff17
ngame <ngame@DESKTOP-ESFCCS8>
2018-04-13 21:04:22 +0430

    -Updated search experience.

--- GoogleMapsUnofficial/View/OnMapControls/SearchUserControl.xaml.cs
+++ GoogleMapsUnofficial/View/OnMapControls/SearchUserControl.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using GoogleMapsUnofficial.ViewModel.GeocodControls;
 using GoogleMapsUnofficial.ViewModel.PlaceControls;
+using GoogleMapsUnofficial.ViewModel.SettingsView;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Contacts;
 using Windows.Devices.Geolocation;
 using Windows.UI.Core;
 using Windows.UI.Popups;
@@ -58,11 +60,34 @@ namespace GoogleMapsUnofficial.View.OnMapControls
                 {
                     if (SearchResults == null) SearchResults = new ObservableCollection<PlaceAutoComplete.Prediction>();
                     SearchResults.Clear();
-                    var s = await PlaceAutoComplete.GetAutoCompleteResults(searchExpression, location: MapView.MapControl.Center, radius: 50000);
-                    if (s == null) return;
-                    foreach (var item in s.predictions)
+                    var saved = SavedPlacesVM.GetSavedPlaces();
+                    saved.Where(x => x.PlaceName.Contains(searchExpression));
+                    foreach (var item in saved)
                     {
-                        SearchResults.Add(item);
+                        SearchResults.Add(new PlaceAutoComplete.Prediction() { place_id = $"{item.Latitude},{item.Longitude}", description = "Saved:" + item.PlaceName });
+                    }
+                    if (await SettingsSetters.GetAccessToContcts())
+                    {
+                        var contacts = await ContactManager.RequestStoreAsync(ContactStoreAccessType.AllContactsReadOnly);
+                        if (contacts != null)
+                        {
+                            var search = await contacts.FindContactsAsync(searchExpression);
+                            var res = search.Where(x => x.Addresses.Count > 0);
+                            foreach (var item in res)
+                            {
+                                var cadd = item.Addresses.FirstOrDefault();
+                                SearchResults.Add(new PlaceAutoComplete.Prediction() { description = "Contacts:" + item.Name, place_id = $"{cadd.StreetAddress}, {cadd.Locality}" });
+                            }
+                        }
+                    }
+                    if (searchExpression.Length >= 3)
+                    {
+                        var s = await PlaceAutoComplete.GetAutoCompleteResults(searchExpression, location: MapView.MapControl.Center, radius: 50000);
+                        if (s == null) return;
+                        foreach (var item in s.predictions)
+                        {
+                            SearchResults.Add(item);
+                        }
                     }
                 });
 
@@ -129,6 +154,63 @@ namespace GoogleMapsUnofficial.View.OnMapControls
         {
             var select = (args.SelectedItem as PlaceAutoComplete.Prediction);
             if (select == null) return;
+            if (select.description.StartsWith("Saved:"))
+            {
+                SearchBox.Text = "";
+                var loc = select.place_id.Split(',');
+                var lat = loc[0]; var lng = loc[1];
+                MapView.MapControl.Center = new Geopoint(
+                new BasicGeoposition()
+                {
+                    Latitude = Convert.ToDouble(lat),
+                    Longitude = Convert.ToDouble(lng)
+                });
+                MapView.StaticMapView.SearchResultPoint = new Geopoint(
+                    new BasicGeoposition()
+                    {
+                        Latitude = Convert.ToDouble(lat),
+                        Longitude = Convert.ToDouble(lng)
+                    });
+                (DataContext as ViewModel).SearchResults.Clear();
+                SearchBox.Text = "";
+                return;
+            }
+            if (select.description.StartsWith("Contacts:"))
+            {
+                var Addres = await SearchHelper.TextSearch(select.place_id);
+                if (Addres != null)
+                {
+                    if (Addres.results != null && Addres.results.Any())
+                    {
+                        var loc = Addres.results.FirstOrDefault().geometry.location;
+                        MapView.MapControl.Center = new Geopoint(
+                            new BasicGeoposition()
+                            {
+                                Latitude = loc.lat,
+                                Longitude = loc.lng
+                            });
+                        MapView.StaticMapView.SearchResultPoint = new Geopoint(
+                            new BasicGeoposition()
+                            {
+                                Latitude = loc.lat,
+                                Longitude = loc.lng
+                            });
+                        (DataContext as ViewModel).SearchResults.Clear();
+                        SearchBox.Text = "";
+                    }
+                    //else
+                    //{
+                    //    var rgc = await ReverseGeoCode.GetLocation(Where);
+                    //    Map.Center = rgc;
+                    //}
+                }
+                //else
+                //{
+                //    var rgc = await ReverseGeoCode.GetLocation(Where);
+                //    Map.Center = rgc;
+                //}
+                return;
+            }
             var res = await GeocodeHelper.GetInfo(select.place_id);
             SearchBox.Text = "";
             if (res == null || res.results.Length == 0)
@@ -157,13 +239,10 @@ namespace GoogleMapsUnofficial.View.OnMapControls
 
         private void Control2_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
-            if (sender.Text.Length >= 3)
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput || args.Reason == AutoSuggestionBoxTextChangeReason.ProgrammaticChange)
             {
-                if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput || args.Reason == AutoSuggestionBoxTextChangeReason.ProgrammaticChange)
-                {
-                    if (SearchBox.Text != string.Empty)
-                        (this.DataContext as ViewModel).SuggestForSearch((sender as AutoSuggestBox).Text);
-                }
+                if (SearchBox.Text != string.Empty)
+                    (this.DataContext as ViewModel).SuggestForSearch((sender as AutoSuggestBox).Text);
             }
         }
 
